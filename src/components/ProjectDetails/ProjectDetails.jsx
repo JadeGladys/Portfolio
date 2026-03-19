09ae37ef8b411da75501e00d28c1a90d58a209fe
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-03-20 01:55:34 +0200

    Updated skills in Readme Exporter

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -23,12 +23,3 @@ export const ProjectDetails = () => {
   )
 }
 
-
-{/* <section className={styles.container} id="projects">
-      <h2 className={styles.title}>Projects</h2>
-      <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
-      </div>
-    </section> */}
